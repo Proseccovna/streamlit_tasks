b2c8fab8ccbc3bee98be42eebb2318979e04eda2
Tatiana <tatiana_zhukova@inbox.ru>
2023-10-31 22:23:58 +0300

    reloaded again

--- done_tasks/STLT.py
+++ done_tasks/STLT.py
@@ -3,16 +3,13 @@ import pandas as pd
 
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 import streamlit as st
 import plotly_express as px
 from plotly.tools import mpl_to_plotly
-from PIL import Image
-from sklearn.datasets import load_iris # pip install scikit-learn
-from sklearn.decomposition import TruncatedSVD, PCA
-from sklearn.preprocessing import StandardScaler
-from sklearn.preprocessing import LabelEncoder
+from skimage import io
+
+
 
 st.title('Изменение картинки в Streamlit')
 
@@ -23,8 +20,8 @@ top_k = st.sidebar.slider('Выберите количество сингуля�
 uploaded = st.sidebar.file_uploader('Загрузить картинку', type = ['jpg'])
 
 if uploaded is not None:
-    image = Image.open(uploaded)
-    image = np.array(image)
+    
+    image = io.imread(uploaded)
     plt.xticks([])
     plt.yticks([])
     U, sing_values, V = np.linalg.svd(image)
@@ -34,10 +31,10 @@ if uploaded is not None:
     trunc_sigma = sigma[:top_k, :top_k]
     trunc_V = V[:top_k, :]
     image = trunc_U@trunc_sigma@trunc_V
-    image = (image - image.min()) / (image.max() - trunc_image.min())
+    image = (image - image.min()) / (image.max() - image.min())
     parameters = top_k
 else: 
-    image = Image.open('https://upload.wikimedia.org/wikipedia/commons/9/9a/%D0%9D%D0%B5%D1%82_%D1%84%D0%BE%D1%82%D0%BE.png')
+    image = io.imread('https://upload.wikimedia.org/wikipedia/commons/9/9a/%D0%9D%D0%B5%D1%82_%D1%84%D0%BE%D1%82%D0%BE.png')
     plt.xticks([])
     plt.yticks([])
 
